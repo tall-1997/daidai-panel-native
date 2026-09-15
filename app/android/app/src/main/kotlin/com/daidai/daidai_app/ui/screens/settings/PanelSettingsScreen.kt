23ea9ee2f84fb89e87816d4c2af23c4355df5d40
tall-1997 <tall-1997@chaitin.com>
2026-09-15 13:35:34 +0000

    fix(compile): add missing clip import in PanelSettingsScreen

--- app/android/app/src/main/kotlin/com/daidai/daidai_app/ui/screens/settings/PanelSettingsScreen.kt
+++ app/android/app/src/main/kotlin/com/daidai/daidai_app/ui/screens/settings/PanelSettingsScreen.kt
@@ -1,6 +1,7 @@
 package com.daidai.daidai_app.ui.screens.settings
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clip
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
